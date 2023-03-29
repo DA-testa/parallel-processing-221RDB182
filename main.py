58b0222be66329691e528f720bcd2adcfc4c7ce0
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-29 13:08:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,36 @@
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+    # Initialize priority queue with first n jobs
+pq = [(0, i) for i in range(n)]
+heapq.heapify(pq)
 
-    return output
+for i in range(m):
+    # Get next available thread
+    t, thread = heapq.heappop(pq)
+    # Calculate start time of current job
+    start_time = t
+    # Calculate finish time of current job
+    finish_time = t + data[i]
+    # Append result to output list
+    output.append((thread, start_time))
+    # Push next job onto priority queue with updated finish time
+    heapq.heappush(pq, (finish_time, thread))
+
+return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
     n = 0
     m = 0
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+   # Call parallel_processing function
+result = parallel_processing(n, m, data)
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
+# Print results
+for thread, start_time in result:
+    print(thread, start_time)
 
 
 
