ab31bd976b9e0dfa633e3fe323d6c0bbb558df6d
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-29 13:24:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-# python3
 import heapq
 
 def parallel_processing(n, m, data):
-output = []
-# Initialize priority queue with first n jobs
-pq = [(0, i) for i in range(n)]
-heapq.heapify(pq)
+    output = []
+    pq = [(0, i) for i in range(n)]
+    heapq.heapify(pq)
 
-for i in range(m):
-    # Get next available thread
-    t, thread = heapq.heappop(pq)
-    # Calculate start time of current job
-    start_time = t
-    # Calculate finish time of current job
-    finish_time = t + data[i]
-    # Append result to output list
-    output.append((thread, start_time))
-    # Push next job onto priority queue with updated finish time
-    heapq.heappush(pq, (finish_time, thread))
+    for i in range(m):
+        t, thread = heapq.heappop(pq)
+        start_time = t
+        finish_time = t + data[i]
+        output.append((thread, start_time))
+        heapq.heappush(pq, (finish_time, thread))
 
-return output
+    return output
 
 def main():
-# Get input from user
-n, m = map(int, input().split())
-data = list(map(int, input().split()))
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
-# Call parallel_processing function
-result = parallel_processing(n, m, data)
+    result = parallel_processing(n, m, data)
 
-# Print results
-for thread, start_time in result:
-    print(thread, start_time)
-    
+    for thread, start_time in result:
+        print(thread, start_time)
     
 if __name__ == "__main__":
     main()
